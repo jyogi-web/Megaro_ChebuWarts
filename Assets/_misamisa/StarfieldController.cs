d782bf42a691a01fb237273ae7f3ae7c2e85e6c6
MisamisaK <misamisa.k.06.05@gmail.com>
2026-03-17 23:45:47 +0900

    星空を映し出すことに成功

--- Assets/_misamisa/StarfieldController.cs
+++ Assets/_misamisa/StarfieldController.cs
@@ -8,16 +8,23 @@ public class StarfieldController : MonoBehaviour
     public float spawnRadius = 50f;
     public float rotationSpeed = 0.5f;
 
+    [Header("Touch Effect")]
+    public GameObject starBurstPrefab;
+    public AudioClip starTouchSE;
+    private AudioSource audioSource;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         SpawnStars();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 星空全体をゆっくり回転
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
     }
 
     void SpawnStars()
@@ -31,6 +38,35 @@ public class StarfieldController : MonoBehaviour
             // 星のサイズをランダムに
             float size = Random.Range(0.05f, 0.2f);
             star.transform.localScale = Vector3.one * size;
+
+            // 各星にStarTouchableをアタッチ
+            star.AddComponent<StarTouchable>().Init(this);
         }
+    }
+
+    // 星が触られたときに呼ばれる
+    public void OnStarTouched(Vector3 position)
+    {
+        if (starBurstPrefab)
+            Instantiate(starBurstPrefab, position, Quaternion.identity);
+
+        if (starTouchSE && audioSource)
+            audioSource.PlayOneShot(starTouchSE);
+    }
+}
+// 各星にアタッチするクラス
+public class StarTouchable : MonoBehaviour
+{
+    private StarfieldController controller;
+
+    public void Init(StarfieldController ctrl)
+    {
+        controller = ctrl;
+    }
+
+    public void Touch()
+    {
+        controller.OnStarTouched(transform.position);
+        Destroy(gameObject, 0.1f);
     }
 }
