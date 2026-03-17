e3138893595dc8028199126186d32904f5441c3c
MisamisaK <misamisa.k.06.05@gmail.com>
2026-03-18 01:27:06 +0900

    地面の追加、 MagicCircleControllerの骨組みづくり

--- Assets/_misamisa/MagicCircleController.cs
+++ Assets/_misamisa/MagicCircleController.cs
@@ -8,6 +8,28 @@ using UnityEngine;
 
 public class MagicCircleController : MonoBehaviour
 {
+    [Header("Magic Circle")]
+    public GameObject magicCircleObject;
+    public GameObject centerStar;
+    public float expandDuration = 1.5f;
+
+    [Header("Controller Settings")]
+    public float activationDistance = 0.6f;
+    public float grabRadius = 0.15f;
+
+    [Header("OVR Anchors")]
+    public Transform leftControllerTransform;
+    public Transform rightControllerTransform;
+
+    [Header("SE")]
+    public AudioClip summonSE;
+    public AudioClip grabSE;
+    public AudioClip fumbleSE;
+    private AudioSource audioSource;
+
+    [Header("Title")]
+    public GameObject titleTextObject;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
