ea15d7adc7a40cf54542e9d87e6fe28cbacaf4ec
MisamisaK <misamisa.k.06.05@gmail.com>
2026-03-17 21:53:06 +0900

    星の設定変数を追加

--- Assets/_misamisa/StarfieldController.cs
+++ Assets/_misamisa/StarfieldController.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class StarfieldController : MonoBehaviour
 {
+    [Header("Starfield Settings")]
+    public GameObject starPrefab;
+    public int starCount = 200;
+    public float spawnRadius = 50f;
+    public float rotationSpeed = 0.5f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
